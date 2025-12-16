143acbb1e6630af4fc4e429b0ed326f79ce82556
kennedyngigi4 <kennedyngigi4@gmail.com>
2025-12-16 09:16:48 +0300

    International calculator view

--- apps/international/views.py
+++ apps/international/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from apps.accounts.models import *
+from apps.international.models import *
 # Create your views here.
+
+
+
+class InternationalCalculator(APIView):
+
+    def post(self, request):
+        pass
+
+    
+
+
